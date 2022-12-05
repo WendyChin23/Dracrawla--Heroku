4b44ab6d5fb112e44f59795f23711c4704b31508
WendyChin23 <kristinevelasquez23@gmail.com>
2022-12-05 22:10:42 +0800

    Revert "edit own details"
    This reverts commit 81855700459f6598b309e9b99bb78646dc9c1917.

--- dracrawla/views.py
+++ dracrawla/views.py
@@ -297,7 +297,6 @@ class ManageAccounts(View):
                 account = Account.objects.filter(uid=Idn).delete()
 
                 print('recorded deleted')
-                return redirect('dracrawla:logout_view')  
     
 
         return redirect('dracrawla:manageaccounts_view')    
